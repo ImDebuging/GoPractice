46e340413c3d1d09c9370f67a3fe81015993bd04
MortySmith <1612945690@qq.com>
2023-12-28 15:00:31 +0800

    go语言中协程和主线程的关系，主死从随

--- goroutine_2023_12_28_1.go
+++ goroutine_2023_12_28_1.go
@@ -3,12 +3,15 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
 func main() {
-	go func() {
-		fmt.Println(1)
-	}() //可以使用匿名函数创建协程
+	for i := 1; i <= 5; i++ {
+		go func(n int) { //可以使用匿名函数创建协程}
+			fmt.Println("协程" + strconv.Itoa(n))
+		}(i) //为了达到预期效果（一个协程对应一个id），要避免这里的资源共享问题
+	}
 	time.Sleep(time.Second) //创建协程以后，主线程要等待协程执行完毕，所以要等待一秒
 }
