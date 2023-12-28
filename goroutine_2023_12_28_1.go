233e2272c4d6cc3396b131e03ffb47ba9f587673
MortySmith <1612945690@qq.com>
2023-12-28 14:36:24 +0800

    go语言中协程和主线程的关系，主死从随

---
+++ goroutine_2023_12_28_1.go
@@ -0,0 +1,14 @@
+// go语言中协程和主线程的关系，“主死从随”
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go func() {
+		fmt.Println(1)
+	}()
+	time.Sleep(time.Second) //创建协程以后，主线程要等待协程执行完毕，所以要等待一秒
+}
